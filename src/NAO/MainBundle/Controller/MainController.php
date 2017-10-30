2ccaf473977575c25e5bda349dad7d7b418a3c94
ChuGui <gchurlet@gmail.com>
2017-10-30 12:35:31 +0100

    Formulaire de connexion: Commencement

--- src/NAO/MainBundle/Controller/MainController.php
+++ src/NAO/MainBundle/Controller/MainController.php
@@ -4,10 +4,10 @@ namespace NAO\MainBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class DefaultController extends Controller
+class MainController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('NAOMainBundle:Default:index.html.twig');
+        return $this->render('NAOMainBundle:Main:home.html.twig');
     }
 }
