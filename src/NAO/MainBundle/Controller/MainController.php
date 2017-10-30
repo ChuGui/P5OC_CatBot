3e801daba8e8d090ceb200571811fbcd1af3dcc7
ChuGui <gchurlet@gmail.com>
2017-10-30 13:51:53 +0100

    Création page Observation. Création roles: USER,NATURALISTE,ADMIN

--- src/NAO/MainBundle/Controller/MainController.php
+++ src/NAO/MainBundle/Controller/MainController.php
@@ -3,11 +3,24 @@
 namespace NAO\MainBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 
 class MainController extends Controller
 {
-    public function indexAction()
+    public function homeAction()
     {
         return $this->render('NAOMainBundle:Main:home.html.twig');
+    }
+
+    public function observationAction(Request $request)
+    {
+        // On vérifie que l'utilisateur dispose bien du rôle ROLE_USER
+        if (!$this->get('security.authorization_checker')->isGranted('ROLE_USER')) {
+            // Sinon on déclenche une exception « Accès interdit »
+            throw new AccessDeniedException('Accès limité aux utilisateurs.');
+        }else{
+            return $this->render('NAOMainBundle:Main:observation.html.twig');
+        }
     }
 }
