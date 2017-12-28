c670be6258a3bd6d5f5b8c7f2389fcc1142696f1
ChuGui <gchurlet@gmail.com>
2017-12-28 17:55:34 +0100

    Partie modal

--- web/js/admin.js
+++ web/js/admin.js
@@ -1,3 +1,24 @@
 $(document).ready(function() {
+    console.log('fichier admin.js chargé');
+    $('#confirmDeleteUserBtn').on('click', function(e) {
 
+        var $confirmBtn = $(e.currentTarget);
+        var userId = $confirmBtn.data('userid');
+        console.log(userId);
+        /*$.ajax({
+            url: $form.data('url'),
+            type: "POST",
+            dataType: "application/json",
+            data: { content: $content },
+            success: function(data) {
+                console.log(data)
+            },
+            error: function(){
+
+                console.log('erreurData ');
+            }
+        })*/
+
+
+    });
 });
