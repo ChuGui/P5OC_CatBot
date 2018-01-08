b865a6b6cd9ce2662962f1626a340694fc1fe068
romain <harlequin.crest@gmail.com>
2018-01-08 19:26:34 +0100

    CSS: Transition Floating labels + CSS html select et type=number

--- web/js/main.js
+++ web/js/main.js
@@ -14,17 +14,30 @@ $(document).ready(function(){
 
 
 	// FLOATING LABELS
-	$( "input[placeholder], textarea[placeholder]" ).focus(function() {
+	$( "input[placeholder], textarea[placeholder], select[placeholder]" ).focus(function() {
 		var floatingLabelValue = $(this).attr('placeholder');
-		$(this).removeAttr('placeholder');
+		$(this).attr('placeholder', '');
 		$(this).prev().text(floatingLabelValue).fadeIn(500);
 	});
 
-	$( "input[placeholder], textarea[placeholder]" ).focusout(function() {
+	$( "input[placeholder], textarea[placeholder], select[placeholder]" ).focusout(function() {
 		floatingLabelValue = $(this).prev().text();
 		$(this).prev().fadeOut(500);
-		$(this).attr('placeholder', floatingLabelValue);
+		$(this).fadeIn(500).delay(500).queue(function(next) { $(this).attr('placeholder', floatingLabelValue); next(); });
+
+
+		$(this).find('.img_first').fadeOut(300, function() {
+	        $(this).attr("src",$(this).attr("data-original-b")).delay(300);
+		    }).fadeIn(300);
+		    return false;		
 	});
+
+	$( "select[placeholder]" ).click(function() {
+		var floatingLabelValue = $(this).prev().text();
+		$(this).attr('placeholder', '');
+		$(this).prev().text(floatingLabelValue).fadeIn(500);
+	});
+
 
 
 	// SVG WINGS FLAPPING
